6deacb7772854fbc1cedd6e74a49136d09c9c1ba
killnetwork12 <aliksavin92@gmail.com>
2025-07-27 22:49:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleDescription");
+    const moreInfo = document.getElementById("moreInfo");
+
+    // Установка громкости
+    backgroundMusic.volume = 0.5; } // Установите громкость на 50% 
